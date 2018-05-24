94d2ffcc320f88d0c982c3a2a04e48724cca9e06
Viri <Viri@DESKTOP-N9L019U>
2018-05-23 20:07:15 -0500

    VRG V -0.0.3-SNAPSHOT Inyeccion de objetos video 7

---
+++ StandAloneSpring/src/main/java/com/mitocode/beans/Pais.java
@@ -0,0 +1,22 @@
+package com.mitocode.beans;
+
+public class Pais {
+	
+	private String pais;
+    private Ciudad ciudad;
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	public Ciudad getCiudad() {
+		return ciudad;
+	}
+	public void setCiudad(Ciudad ciudad) {
+		this.ciudad = ciudad;
+	}
+	
+	
+
+}
--- StandAloneSpring/src/main/java/com/mitocode/beans/Persona.java
+++ StandAloneSpring/src/main/java/com/mitocode/beans/Persona.java
@@ -5,34 +5,33 @@ public class Persona {
 	private int id;
 	private String name;
 	private String 	apodo;
-	
-	public Persona(int id, String name,String apodo){
-		this.id=id;
-		this.name=name;
-		this.apodo=apodo;
-	}
-	
-	public Persona(int id){
-		this.id=id;
-	}
-	
-	public Persona(String apodo){
-		this.apodo=apodo;
-	}
+	private Pais pais;
 	
 	public int getId() {
 		return id;
 	}
-
-	
+	public void setId(int id) {
+		this.id = id;
+	}
 	public String getName() {
 		return name;
 	}
-
-	
+	public void setName(String name) {
+		this.name = name;
+	}
 	public String getApodo() {
 		return apodo;
 	}
-
+	public void setApodo(String apodo) {
+		this.apodo = apodo;
+	}
+	public Pais getPais() {
+		return pais;
+	}
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
 	
-}
+	
+	
+	}
